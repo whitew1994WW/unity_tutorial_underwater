dd1e18addb81befaef384091027b61400716a0ed
whitew1994WW <will.white@dativa.com>
2021-01-25 11:24:26 +0000

    Whole project first commit

---
+++ Assets/scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("loadFirstScene", 10f); 
+    }
+
+    void loadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms-1")] [SerializeField] float moveScaling = 2;
+    [SerializeField] float maxXRange = 5f;
+    [SerializeField] float minYRange = -5f;
+    [SerializeField] float maxYRange = 5f;
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float controlYawFactor = -30f;
+    Vector3 moveThrow;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = positionPitchFactor*transform.localPosition.y + (controlPitchFactor*moveThrow.y);
+        float yaw = positionYawFactor*transform.localPosition.x;
+        float roll = controlYawFactor * moveThrow.x;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        moveThrow = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"),
+                                        CrossPlatformInputManager.GetAxis("Vertical"), 0);
+
+        Vector3 moveOffset = Time.deltaTime * moveThrow * moveScaling;
+
+        Vector3 newPos = transform.localPosition + moveOffset;
+        newPos.x = Mathf.Clamp(newPos.x, -maxXRange, maxXRange);
+        newPos.y = Mathf.Clamp(newPos.y, minYRange, maxYRange);
+        transform.localPosition = newPos;
+    }
+}
